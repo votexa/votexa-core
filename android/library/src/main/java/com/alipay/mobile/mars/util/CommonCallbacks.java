e65c149d85b25667d7af974c6399b5cd3f214ba7
errnull <errnull@libsvg.com>
2025-04-13 19:57:41 +0800

    feat: votexa init.

--- android/library/src/main/java/com/alipay/mobile/mars/util/CommonCallbacks.java
+++
@@ -1,9 +0,0 @@
-package com.alipay.mobile.mars.util;
-
-public class CommonCallbacks {
-
-    public interface SuccessErrorCallback {
-        void onResult(boolean success, String errMsg);
-    }
-
-}
--- resources/benchmark.md
+++
@@ -1,5 +0,0 @@
-## Alipay homepage atmosphere animation
-![AnimationImage](1.png)
-![DataImage](2.png)
-
-We will provide more performance test data for animations in the future, so stay tuned.
